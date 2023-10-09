34ef3502df7e34ec46e875d8249b5aa8d4d4a4c4
out0f4mm0 <eriking2004@gmail.com>
2023-10-09 09:55:07 +0200

    Started exercise 7

---
+++ js/Exercise07.js
@@ -0,0 +1,10 @@
+class Caesar{
+    constructor(displacementNumber = 1) {
+        this.displacementNumber = displacementNumber
+    }
+    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l', 'ñ','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    encode(textToEncode = "hello"){
+        const textToEncodeLowerCase = textToEncode.toLowerCase()
+    }
+}
+const caesar = new Caesar(22)
